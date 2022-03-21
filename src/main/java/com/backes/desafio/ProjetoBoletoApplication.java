9669e9a5efe8ded24c8e1b4fc822efc260c8d15e
Daiane Backes <daianebackes96@gmail.com>
2022-03-21 16:47:23 -0300

    Primeiro commit boleto

---
+++ src/main/java/com/backes/desafio/ProjetoBoletoApplication.java
@@ -0,0 +1,13 @@
+package com.backes.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBoletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBoletoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backes/desafio/ProjetoBoletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backes.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBoletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
