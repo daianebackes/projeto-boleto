f6fae4dd8c32ec05a82baf7367d19dd1ec8fde04
Daiane Backes <daianebackes96@gmail.com>
2022-04-19 16:28:15 -0300

    criando lista de boletos

--- src/main/java/com/backes/desafio/services/BoletoService.java
+++ src/main/java/com/backes/desafio/services/BoletoService.java
@@ -1,11 +1,15 @@
 package com.backes.desafio.services;
 
+import com.backes.desafio.dtos.BoletoDto;
 import com.backes.desafio.dtos.NovoBoletoDto;
 import com.backes.desafio.dtos.RetornoNovoBoletoDto;
 import com.backes.desafio.model.BoletoModel;
 import com.backes.desafio.model.StatusBoleto;
 import com.backes.desafio.repositories.BoletoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BoletoService {
@@ -33,5 +37,22 @@ public class BoletoService {
         retornoDto.setDataVencimento(modelDb.getDataVencimento());
         retornoDto.setStatus(modelDb.getStatus());
         return retornoDto;
+    }
+
+    public List<BoletoDto> buscarTodos() {
+       List<BoletoModel> listaBoleto = boletoRepository.findAll();
+       List<BoletoDto> listaBoletoDto = new ArrayList<>();
+       for(BoletoModel boletoModel : listaBoleto) {
+           BoletoDto boletoDto = new BoletoDto();
+           boletoDto.setConsumidor(boletoModel.getConsumidor());
+           boletoDto.setDataPagamento(boletoModel.getDataPagamento());
+           boletoDto.setId(boletoModel.getId());
+           boletoDto.setStatus(boletoModel.getStatus());
+           boletoDto.setTotalEmCentavos(boletoModel.getTotalEmCentavos());
+           boletoDto.setDataVencimento(boletoModel.getDataVencimento());
+
+           listaBoletoDto.add(boletoDto);
+       }
+       return listaBoletoDto;
     }
 }
